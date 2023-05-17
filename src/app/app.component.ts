1a712e21e3a443d1d679eb1b4688d7507542e493
vmanasa0398 <133651174+vmanasa0398@users.noreply.github.com>
2023-05-17 12:03:19 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Book } from './shared/models/book-response.model';
+import { ApiService } from './core/services/api.service';
 
 @Component({
-  selector: 'front-end-internship-assignment-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Books Library App';
+  searchText: string = '';
+  booksList: Book[] = [];
+  loading: boolean = false;
+  error: string = '';
+  page: number = 1;
+  limit: number = 10;
+
+  constructor(private apiService: ApiService) {}
+
+  searchBooks() {
+    if (this.searchText.trim() !== '') {
+      this.loading = true;
+      this.error = '';
+
+      this.apiService.searchBooks(this.searchText, this.page, this.limit).subscribe(
+        (response) => {
+          this.booksList = response.docs;
+          this.loading = false;
+        },
+        (error) => {
+          this.error = 'An error occurred while fetching the search results.';
+          this.loading = false;
+        }
+      );
+    }
+  }
+
+  clearSearch() {
+    this.searchText = '';
+    this.booksList = [];
+    this.page = 1;
+  }
+
+  previousPage() {
+    this.page--;
+    this.searchBooks();
+  }
+
+  nextPage() {
+    this.page++;
+    this.searchBooks();
+  }
 }
